eb5876611ffcf5fc7be4c4f6b3a755cc7759a754
Alexander Nosov <trashgenerator@gmail.com>
2017-03-05 12:40:01 +0100

    discrict ids

--- src/FrankfurtMap/FrankfurtMap.js
+++ src/FrankfurtMap/FrankfurtMap.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 import _ from 'lodash';
 import './map.css';
 
-const COLOR = '#000';
+const COLOR = '#661ecb';
 
 class FrankfurtMap extends Component {
   colorDistricts = () => {
